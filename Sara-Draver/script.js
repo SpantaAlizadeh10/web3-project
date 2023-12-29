dc908bba8a2f5ce4fd09d53ccb2a83c8016103b5
Sepanta.js <147656655+SpantaAlizadeh10@users.noreply.github.com>
2023-12-29 12:26:47 -0800

    SaraDriver

---
+++ Sara-Draver/script.js
@@ -0,0 +1,11 @@
+
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+const isTired = false;
+
+if(hasDriversLicense && hasGoodVision && !isTired){
+    console.log('Sara is able to Drive!')
+} else{
+    console.log('Someone else should drive...');
+}
