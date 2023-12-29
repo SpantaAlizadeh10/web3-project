609c6fb4c3f2d13e6aaf1a8487a0190cce2d395b
Sepanta.js <147656655+SpantaAlizadeh10@users.noreply.github.com>
2023-12-29 11:34:16 -0800

    Money-Heaight

---
+++ money-heaight/money-height.js
@@ -0,0 +1,15 @@
+const money = 100;
+if (money){
+    console.log("Dont spend it all;)");
+}else{
+    console.log('You should get a job!');
+}
+
+
+let height = 50000;
+if(height){
+    console.log("Height is defined");
+
+}else{
+    console.log('Height is undefined');
+}
