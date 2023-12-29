3656f438eecb37654e6f034636b0c2bae81dedb6
Sepanta.js <147656655+SpantaAlizadeh10@users.noreply.github.com>
2023-12-29 10:41:23 -0800

    Average Calculate

---
+++ Average.js
@@ -0,0 +1,9 @@
+ const now=2023;
+ const ageSara = now-1982;
+ const ageAli = now-2000;
+
+ console.log(now-1982 > now-2000);
+
+ const averageAge = (ageSara + ageAli) / 2
+
+ console.log(ageAli,ageSara,"===>",averageAge);
