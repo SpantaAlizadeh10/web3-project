7060811e7d1c4c0f8f72e013b858c9ddd69be4f4
Sepanta.js <147656655+SpantaAlizadeh10@users.noreply.github.com>
2023-12-29 12:00:17 -0800

    Amazing Number

---
+++ amzing-Number/script.js
@@ -0,0 +1,12 @@
+
+if (favourit ===23){
+    console.log('Cool! 23 is an amazing Number!')
+} else if (favourit === 8){
+    console.log(' 8 is also a cool Number')
+} else if(favourit === 10){
+    console.log('10 is also a good Number')
+} else {
+    console.log('Number is not 23 or 8 or 10?');
+}
+
+if(favourit !==23) console.log('why not 23?');
