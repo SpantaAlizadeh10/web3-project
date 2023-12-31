e60b44f5ddd304ed16a97c1a21f4eed1d4f5ede4
Sepanta.js <147656655+SpantaAlizadeh10@users.noreply.github.com>
2023-12-31 11:49:56 -0800

    fruit processeor

---
+++ fruit processeor/script.js
@@ -0,0 +1,14 @@
+function fruitProcessor(apple, oranges){
+    const juice = `juice with ${apple} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleguice = fruitProcessor(10, 20);
+console.log(appleguice);
+
+
+const appleOrangeJuice = fruitProcessor(2, 10);
+console.log(appleOrangeJuice);
+
+const num =Number('28');
+console.log(num);
