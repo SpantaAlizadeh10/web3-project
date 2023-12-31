1d7f607a76002d88493f49121fa4082612f2127c
Sepanta.js <147656655+SpantaAlizadeh10@users.noreply.github.com>
2023-12-31 12:07:45 -0800

    two function

---
+++ two function/script.js
@@ -0,0 +1,15 @@
+function calAge1(birthday){
+    return 2021 - birthday;
+}
+
+const age1= calAge1(1993);
+
+
+
+
+
+const calAge2 = function (birthday){
+return 2035 - birthday;
+}
+const age2 = calAge2(1995);
+console.log(age1, age2);
